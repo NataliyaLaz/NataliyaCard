800c63d29a3f04b2767d851afaf77fbc3f431641
NataliyaLaz <nataliya.laz@gmail.com>
2022-03-15 20:57:03 +0300

    Initial Commit

---
+++ NataliyaCard/NataliyaCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  NataliyaCardApp.swift
+//  NataliyaCard
+//
+//  Created by Nataliya Lazouskaya on 15.03.22.
+//
+
+import SwiftUI
+
+@main
+struct NataliyaCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
