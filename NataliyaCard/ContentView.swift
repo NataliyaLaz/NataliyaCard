d73043aec6f94812a4308b8d8f743e0766d473b2
NataliyaLaz <nataliya.laz@gmail.com>
2022-03-17 22:19:40 +0300

    business card ready

--- NataliyaCard/ContentView.swift
+++ NataliyaCard/ContentView.swift
@@ -9,8 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 0.09, green: 0.63, blue: 0.52, opacity: 0.6)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("nataliya")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 150.0, height: 150.0)
+                    .clipShape(Circle())
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 3)
+                    )
+                Text("Nataliya Lazouskaya")
+                    .font(Font.custom("Pacifico-Regular", size: 35))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                InfoView(text: "+375 29 7153353", imageName: "phone.fill")
+                InfoView(text: "nataliya.laz@gmail.com", imageName: "envelope.fill")
+
+                    
+            }
+        }
     }
 }
 
@@ -19,3 +43,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
---
+++ NataliyaCard/InfoView.swift
@@ -0,0 +1,35 @@
+//
+//  InfoView.swift
+//  NataliyaCard
+//
+//  Created by Nataliya Lazouskaya on 17.03.22.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.white)
+            .frame(height: 50)
+            .overlay(
+                HStack {
+                    Image(systemName: imageName)
+                        .foregroundColor(.green)
+                    Text(text)
+                }
+            )
+            .padding(.all)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "Hello", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
